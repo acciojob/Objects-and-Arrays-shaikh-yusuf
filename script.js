4e5aaed8a3758310659d94c18467b93eee5a8b68
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-18 16:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
+
+// Create a reference to the array players
+const team = players;
+
+// Create a copy of the array players using the spread operator
+const team1 = [...players];
+
+// Create a copy of the object person using the spread operator
+const cap1 = { ...person };
+
+// Example: Modifying a property in the original players array
+players.push("Sarah");
+
+// Example: Modifying a property in the original person object
+person.name = "Jane Smith";
+
+console.log("team:", team);   // This will show the updated players array (including Sarah)
+console.log("team1:", team1); // This will show the original players array
+console.log("cap1:", cap1);   // This will show the original person object
+
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
